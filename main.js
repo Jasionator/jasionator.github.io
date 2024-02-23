71645a6b0c585167cf1016ce1e2bd652c36ace0c
Jan Badtke <janbadtke@gmail.com>
2024-02-23 20:48:02 +0100

    added some css and js

--- main.js
+++ main.js
@@ -1,32 +1,19 @@
 var rate = [0, 0, 0, 0, 0];
 
+var rateButtons = document.getElementsByClassName("ocena");
+var rateCounts = document.getElementsByClassName("ocena-liczba");
+
 for(let i=0; i<5; i++){
-	document.getElementById("ocena"+(i+1)).addEventListener('click', function(){
-		rate[i]++;
-		this.textContent = rate[i];
-		console.log(rate[i]);
+	rateButtons[i].addEventListener('click', async function(){
+		rateCounts[i].textContent = ++rate[i];
 	});
 }
 
-//old
-/*var ocena1 = document.getElementById("ocena1")
-	.addEventListener('click', ()=>{
-		rate[0]++;
-	});
-var ocena2 = document.getElementById("ocena2")
-	.addEventListener('click', ()=>{
-		rate[1]++;
-	});
-var ocena3 = document.getElementById("ocena3")
-	.addEventListener('click', ()=>{
-		rate[2]++;
-	});
-var ocena4 = document.getElementById("ocena4")
-	.addEventListener('click', ()=>{
-		rate[3]++;
-	});
-var ocena5 = document.getElementById("ocena5")
-	.addEventListener('click', ()=>{
-		rate[4]++;
-	});
-*/
+//confirm page reload
+window.onbeforeunload = function(event){
+    return confirm("Czy napewno chcesz odświeżyć stronę? Możesz utracić super dane z ankiety");
+};
+
+document.addEventListener('contextmenu', event => {
+    event.preventDefault();
+});
